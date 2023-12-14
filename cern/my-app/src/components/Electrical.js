e962601a1bfd68d3c8cbae7a9506257f5c4f010f
AriB2003 <49720892+AriB2003@users.noreply.github.com>
2023-12-14 00:31:47 -0500

    Edit Image Naming

--- cern/my-app/src/components/Electrical.js
+++ cern/my-app/src/components/Electrical.js
@@ -9,18 +9,35 @@ function Electrical() {
         {
             title: 'Sprint 1',
             content: `
-                <p>For Sprint 1, we breadboarded major systems (IMU, ESP-32, h-bridge, inverter) for our first prototype. Based on these tests, we created a PCB schematic that was sent off to fabrication in Sprint 2. The PCB miniaturized the system, allowed for less hassle with wiring, and let us have more functionality. The design includes a full-bridge rectifier and voltage doubler circuit for inductive charging rectification, a linear regulator, and a series of MOSFETs for ground isolation. The MOSFETs decouple their behavior while allowing for the desired behavior of a tilt switch to initially turn the power on to the system.</p>
-    
-                <p>The ESP32 is connected to all peripherals, utilizing every available pin. This included addressing challenges with pins used for strapping in the boot sequence and ensuring proper boot conditions. The layout was miniaturized as much as possible, with attention to component placement, minimization of breaks in the ground plane, and efficient use of board area.</p>
+            For Sprint 1 we breadboarded major systems (IMU, ESP-32, h-bridge, inverter) for our first prototype. Based on these tests, we created a PCB schematic that was sent off to fabrication in Sprint 2. The PCB miniaturized the system, allowed for less hassle with wiring, and let us have more functionality. From right to left in the picture below, the design has a full-bridge rectifier and voltage doubler circuit for the inductive charging rectification. It then has a linear regulator for regulating the voltage down to 5V for the LiPo charger. The ground of the main system is isolated via a series of MOSFETs to prevent floating voltage from feeding forward through the ESP32 GPIO protection circuitry. Having the MOSFETs on separate grounds decouples their behavior while allowing for the desired behavior of a tilt switch being able to initially turn power on to the system and then the ESP32 trigger pin going HIGH to maintain power to the system until shut off when the pin is returned to the LOW state. The use of tilt switches was to retain simplicity and also to prevent parasitic power drain by alternative systems like a low-power Bluetooth chip or a Hall effect sensor and a magnet in the charging base. Finally, a 3.3V regulator powers the main computing and sensing circuitry.
             `,
-            imageUrl: '/path-to-sprint1-electrical-image.jpg' // Replace with actual image path
+            imageUrl: '/images/Charging KiCAD Schematic.png' // Replace with actual image path
+        },
+        {
+            content: `
+            <p>In the below image, the ESP32 is connected to all the peripherals: motor controllers, IMUs, LEDs, a voltage divider for battery readings, and a tilt switch for detecting when the Sphero is tilted to initiate a shutdown timer. Every single available pin is used on the ESP32 chip, which proves to be an interesting dilemma because some pins are used for strapping in the boot sequence and therefore must be in certain states at boot. Some pins cannot be used entirely for digital I/O such as the hardware Serial and the external flash pins labeled SDx. Despite these downsides, the ESP32 is used because we had DevKit boards on-hand for Sprint 1 testing, and the board has fast compute and Bluetooth and WiFi capabilities, which makes it perfect for this application.</p>
+            <p>There are four pins: VP, VN, 34, and 35, which do not have output capabilities and also lack pull-up/down resistors. These are used for the voltage reading, tilt switch reading, and the two fault pins for the motor controller, which are the only four non-output functions of the board. The pull resistors are added because the fault pins have no internal resistors.</p> 
+            <p>Most other GPIO pins are standard, except for 14, 12, 5, 0, 2, and 15. These pins perform various functions from strapping to bootlogging. Pin 14 is a boot pin that streams the bootlog and returns to a HIGH state. As such, it is connected to the motor controller because its sleep pin prevents this pin’s state changes from driving any circuitry. Pin 12 must be LOW for the boot sequence and therefore is connected to the motor driver which has internal pull-down resistors on its inputs, which guarantees this state. Pin 5 must be HIGH for the boot sequence, so it is connected to the LEDs which are common anode and therefore guarantee that pin’s state. Pin 0 must change states between booting and flashing, so it is left disconnected in order to accommodate the possibility of a WiFi bootloader. Pin 2 must be LOW for boot so like pin 12 it is connected to the motor driver. Finally, pin 15 like pin 5 must remain HIGH for the boot sequence so it is connected to the LEDs. Appropriate capacitors and resistors were chosen per the datasheets for the components and pull resistors were standardized to 10k to limit part count.</p>
+                    `,
+            imageUrl: '/images/ESP32 KiCAD Schematic.png' // Replace with actual image path
+        },
+        {
+            content: `
+            The layout of the PCB was miniaturized as much as possible. Since this was Ari’s first time making a PCB, he added many test points, expecting something to go wrong — it worked on the first try. There were a lot of errors he caught in its design before he sent it out for fabrication from improper footprints, to bad pin mappings, to bad nets — all good learnings about what needs to be triple-checked. Some components were constrained location-wise. For instance, the tilt switches tilt side-to-side and therefore have to be in that orientation to prevent false actuations from forward/backwards pitching of the Sphero. The smoothing capacitors had to be as close to their respective chips as possible. And finally, the space under the ESP32 and IMU breakouts was used for components to save on board area. We added various power headers to allow for us to jump power to different parts of the circuit and bypass components. The top plane is at VBAT and is powered through the 3A fuse whereas the bottom plane is GND. 
+            `,
+            imageUrl: '/images/Red PCB KiCAD.png' // Replace with actual image path
+        },
+        {
+            content: `
+            Ari spent a lot of time arranging components to minimize the breaks in the ground plane, and wherever it had a break he made up for it through bridges on both planes to create more ground pathways following major power rails. The final board size is just shy of 60x70mm. The square shape allowed for the best packing, whereas a circle would have been larger as a result of the worse layout characteristics.            `,
+            imageUrl: '/images/Blue PCB KiCAD.png' // Replace with actual image path
         },
         {
             title: 'Sprint 2',
             content: `
                 <p>We populated the PCB and tested induction coils. Despite some issues with the coils, we managed to integrate them into the prototype. The PCBs were populated using a manual pick and place machine and a reflow oven. Challenges included sourcing components and shipping delays from DigiKey, yet the PCB was fully functional on the first try. This process was a learning experience in using solder paste, pick and place machines, and reflowing.</p>
             `,
-            imageUrl: '/path-to-sprint2-electrical-image.jpg' // Replace with actual image path
+            imageUrl: '/images/ESP32 KiCAD Schematic.png' // Replace with actual image path
         },
         {
             title: 'Sprint 3',
